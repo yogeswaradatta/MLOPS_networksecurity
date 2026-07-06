4dd52cc0ea4a4d547ea30fa04395624c7122f0b6
yogeswaradatta <yogeswaradatta123@gmail.com>
2026-07-07 01:30:52 +0530

    deployment1

--- networksecurity/cloud/s3_syncer.py
+++ networksecurity/cloud/s3_syncer.py
@@ -1,11 +1,34 @@
-import os
-
+import subprocess
+from networksecurity.logging.logger import logging
 
 class S3Sync:
-    def sync_folder_to_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync {folder} {aws_bucket_url} "
-        os.system(command)
 
-    def sync_folder_from_s3(self,folder,aws_bucket_url):
-        command = f"aws s3 sync  {aws_bucket_url} {folder} "
-        os.system(command)
+    def sync_folder_to_s3(self, folder, aws_bucket_url):
+        command = ["aws", "s3", "sync", folder, aws_bucket_url]
+
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True
+        )
+
+        logging.info(result.stdout)
+
+        if result.returncode != 0:
+            logging.error(result.stderr)
+            raise Exception(result.stderr)
+
+    def sync_folder_from_s3(self, folder, aws_bucket_url):
+        command = ["aws", "s3", "sync", aws_bucket_url, folder]
+
+        result = subprocess.run(
+            command,
+            capture_output=True,
+            text=True
+        )
+
+        logging.info(result.stdout)
+
+        if result.returncode != 0:
+            logging.error(result.stderr)
+            raise Exception(result.stderr)
