d8576983bb85954ddb5e5e41aef975746d5d506b
yogeswaradatta <yogeswaradatta123@gmail.com>
2026-07-07 12:35:30 +0530

    fina

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -6,7 +6,10 @@ print(training_pipeline.PIPELINE_NAME)
 print(training_pipeline.ARTIFACT_DIR)
 
 class TrainingPipelineConfig:
-    def __init__(self,timestamp=datetime.now()):
+    def __init__(self,timestamp=None):
+        if timestamp is None:
+            timestamp=datetime.now()
+            
         timestamp=timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name=training_pipeline.PIPELINE_NAME
         self.artifact_name=training_pipeline.ARTIFACT_DIR
